f3a18acdf71b0f5a6464e5174e89c066ebe5b8c6
huaxingyunrui <renhanlinwy@163.com>
2019-03-07 15:12:40 +0800

    the first commit

---
+++ PirateIsland/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PirateIsland
+//
+//  Created by huaxingyunrui on 2019/3/7.
+//  Copyright © 2019 PL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PirateIsland/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PirateIsland
+//
+//  Created by huaxingyunrui on 2019/3/7.
+//  Copyright © 2019 PL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

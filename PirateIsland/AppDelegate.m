ca8aef76852b33db115abb6551f37a158fd74ccf
yana <sy@zhihuigongjiang.com>
2019-03-07 15:21:03 +0800

    hello world

--- PirateIsland/AppDelegate.m
+++ PirateIsland/AppDelegate.m
@@ -16,7 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    NSLog(@"hello world");
     return YES;
 }
 

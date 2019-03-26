de4c0639329d5e17481234d46cfb001c8fa39e8d
huaxingyunrui <renhanlinwy@163.com>
2019-03-26 09:10:34 +0800

    国际化

---
+++ PirateIsland/HLUnit/internationaLization/HLBundle.h
@@ -0,0 +1,17 @@
+//
+//  HLBundle.h
+//  bingrui-ios
+//
+//  Created by huaxingyunrui on 2019/3/20.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HLBundle : NSBundle
++ (void)setLanguage:(NSString *)language;
+@end
+
+NS_ASSUME_NONNULL_END
